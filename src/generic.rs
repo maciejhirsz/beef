da283e01be24a6c0a3c57b8d8365e54e42d39ba3
Maciej Hirsz <hello@maciej.codes>
2020-04-15 15:20:16 +0200

    Fix formatting

--- src/generic.rs
+++ src/generic.rs
@@ -12,8 +12,8 @@ use core::ptr::NonNull;
 
 #[cfg(target_pointer_width = "64")]
 use crate::lean::internal::Lean;
-use crate::wide::internal::Wide;
 use crate::traits::{Beef, Capacity};
+use crate::wide::internal::Wide;
 
 /// A clone-on-write smart pointer, mostly compatible with [`std::borrow::Cow`](https://doc.rust-lang.org/std/borrow/enum.Cow.html).
 ///
